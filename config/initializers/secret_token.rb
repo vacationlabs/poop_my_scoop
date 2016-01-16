c8c8476c7ca4bf69787de55c4b1d62a6559173e9
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-01-16 12:10:52 +0530

    -- first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoopMyScoop::Application.config.secret_token = 'f4f261a30f12e2d298c10b6d04a89089ea04b669bd00404cf0d83497ea848ddd98357a5f08b7675f70056cd7a02d66da07cb2a543a612681809398a9816f9dba'
