8440539ef0bee1f9f911c07abb5ec68bb971bb07
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-01-17 11:37:48 +0530

    -- changes done to the pop up image uploader

--- app/assets/javascripts/editorControllers.js
+++ app/assets/javascripts/editorControllers.js
@@ -3,12 +3,17 @@ editorControllers.controller('editorMarkdownController', ['$scope', 'PoopMyScoop
     $scope.edit_content = "this is my data"
     
     console.log($scope.blog_title)
-    $scope.fetch_images_data = function(){
+    $scope.fetch_images_data = function(slice_range){
       var prom = PoopMyScoop.fetch_images($scope.blog_title);
       prom.success(function(data) {
+        
         $scope.images_data = data
+        $scope.images_ranged_data = []
+        while($scope.images_data.length)
+        {
+          $scope.images_ranged_data.push($scope.images_data.splice(0, slice_range))
+        }
         $('#myModal').modal('show')
       });
 
     }
-}])
--- app/models/image_api.rb
+++ app/models/image_api.rb
@@ -3,7 +3,7 @@ class ImageApi
   BASE_URL_PARTIAL2 = ".staticflickr.com"
   BASE_THUMBNAIL_PARTIAL = "_t.jpg"
   BASE_MEDIUM_PARTIAL = ".jpg" # gets image with width 640
-  
+  BASE_SQUARE_PARTIAL = "_s.jpg"
   def initialize
     FlickRaw.api_key="fa375f204ca300fd95aed5e167deece2"
     FlickRaw.shared_secret="ad6ba1b0dc636c5a"
@@ -17,8 +17,10 @@ class ImageApi
   def self.get_t_link(i)
     "#{BASE_URL_PARTIAL1}#{i.farm}#{BASE_URL_PARTIAL2}/#{i.server}/#{i.id}_#{i.secret}#{BASE_THUMBNAIL_PARTIAL}"
   end
-
-   
+  # get square image
+  def self.get_s_link(i)
+    "#{BASE_URL_PARTIAL1}#{i.farm}#{BASE_URL_PARTIAL2}/#{i.server}/#{i.id}_#{i.secret}#{BASE_SQUARE_PARTIAL}"
+  end
 
   # gets medium image link
   def self.get_m_640_link(i)
@@ -49,6 +51,7 @@ class ImageApi
         :id => i.id,
         :image_m_link => ImageApi.get_m_640_link(i),
         :image_t_link => ImageApi.get_t_link(i),
+        :image_s_link => ImageApi.get_s_link(i),
         :title => i.title,
       }
       image_arr_hash.push(img_hash)
