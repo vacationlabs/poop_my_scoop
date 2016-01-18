b0d78c7f6daa58c91bbdebc5686150a58e526e54
Ankita-Palekar <453ankitapalekar@gmail.com>
2016-01-18 20:27:00 +0530

    -- added params if blank

--- app/assets/javascripts/services/image_factory.js
+++ app/assets/javascripts/services/image_factory.js
@@ -12,6 +12,12 @@ angular.module('poopMyScoop').factory('PoopMyScoop', ['$http', function($http) {
 
   PoopMyScoop.fetch_images = function(search_title){
     // TODO see if title containing spaces give correct result
+    
+    if (_.isEmpty(search_title)) {
+      alert("Its empty")
+      return 0
+    };
+
     var prom = $http.get(encodeURI(poop_my_scoop_api + 'fetch_images.json?text=' + search_title))
     prom.success(function(data) {
       PoopMyScoop.images = data;
--- app/controllers/image_api_controller.rb
+++ app/controllers/image_api_controller.rb
@@ -6,7 +6,7 @@ class ImageApiController < ApplicationController
   def fetch_images
     params[:page_count] = 10
     params[:page] = 1 if params[:page].blank?
-    raise "search text not provided " if params[:text].blank?
+    params[:text] = "" if params[:text].blank?
     images_arr = ImageApi.get_images(params)
     respond_to do |format|
       # format.html render nothing: true
